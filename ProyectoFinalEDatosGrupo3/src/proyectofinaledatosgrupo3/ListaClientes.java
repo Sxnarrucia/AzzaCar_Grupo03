194a56aa5d45866d5c2e691cc6819151ec10b53f
Esteban Sanarrucia <esteban9926@hotmail.com>
2023-10-23 21:02:55 -0600

    Metodo modificar creado
    Metodo modificar creado

--- ProyectoFinalEDatosGrupo3/src/proyectofinaledatosgrupo3/ListaClientes.java
+++ ProyectoFinalEDatosGrupo3/src/proyectofinaledatosgrupo3/ListaClientes.java
@@ -1,5 +1,8 @@
 package proyectofinaledatosgrupo3;
 
+import java.awt.HeadlessException;
+import javax.swing.JOptionPane;
+
 public class ListaClientes {
 
     private NodoClientes cabeza;
@@ -58,17 +61,119 @@ public class ListaClientes {
         }
     }
 
-    public void modifica(Cliente cliente) {
-        if (cabeza.getDato().getIdCliente() == cliente.getIdCliente()) {
-            NodoClientes aux = cabeza;
-            aux.setDato(cliente);
+    public Cliente extrae(int id) {
+        Cliente cliente = null;
+        if (cabeza.getDato().getIdCliente() == id) {
+            cliente = cabeza.getDato();
+            return cliente;
         } else {
             NodoClientes aux = cabeza;
-            while (aux.getSiguiente() != null && aux.getSiguiente().getDato().getIdCliente() <= cliente.getIdCliente()) {
+            while (aux.getSiguiente() != null && aux.getSiguiente().getDato().getIdCliente() != id) {
                 aux = aux.getSiguiente();
             }
-            if (aux.getDato().getIdCliente() == cliente.getIdCliente()) {
-                aux.setDato(cliente);
+            cliente = aux.getSiguiente().getDato();
+            return cliente;
+        }
+    }
+
+    public void modificar(int id) {
+        int opcion = 0;
+        if (cabeza == null) {
+            System.out.println("La lista se encuentra vacía");
+        } else if (cabeza.getDato().getIdCliente() == id) {
+            try {
+                opcion = Integer.parseInt(JOptionPane.showInputDialog("¿Qué desea modificar?\n"
+                        + " 1 = Primer Nombre del Cliente\n "
+                        + " 2 = Apellidos del Cliente\n "
+                        + " 3 = Cedula del Cliente\n "
+                        + " 4 = Correo del Cliente\n "
+                        + " 5 = Numero del Cliente\n "));
+            } catch (HeadlessException | NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Ingreso opcion incorrecta");
+            }
+            switch (opcion) {
+                case 1 -> {
+                    String nombre = JOptionPane.showInputDialog("Ingrese nuevo nombre");
+                    cabeza.getDato().setNombreCliente(nombre);
+                    JOptionPane.showMessageDialog(null, "Cambiado con exito");
+                }
+                case 2 -> {
+                    String apellidos = JOptionPane.showInputDialog("Ingrese nuevos apellidos");
+                    cabeza.getDato().setApellidosCliente(apellidos);
+                    JOptionPane.showMessageDialog(null, "Cambiado con exito");
+                }
+                case 3 -> {
+                    int cedula = Integer.parseInt(JOptionPane.showInputDialog("Ingrese nueva cedula"));
+                    cabeza.getDato().setCedulaCliente(cedula);
+                    JOptionPane.showMessageDialog(null, "Cambiado con exito");
+                }
+                case 4 -> {
+                    String correo = JOptionPane.showInputDialog("Ingrese nuevo correo");
+                    cabeza.getDato().setCorreoCliente(correo);
+                    JOptionPane.showMessageDialog(null, "Cambiado con exito");
+                }
+                case 5 -> {
+                    int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese nuevo numero"));
+                    cabeza.getDato().setNumCliente(numero);
+                    JOptionPane.showMessageDialog(null, "Cambiado con exito");
+                }
+            }
+        } else {
+            NodoClientes aux = cabeza;
+            while (aux.getSiguiente() != null && aux.getSiguiente().getDato().getIdCliente() <= id) {
+                aux = aux.getSiguiente();
+            }
+            if (aux.getDato().getIdCliente() == id) {
+                try {
+                    opcion = Integer.parseInt(JOptionPane.showInputDialog("¿Qué desea modificar?\n"
+                            + " 1 = Primer Nombre del Cliente\n "
+                            + " 2 = Apellidos del Cliente\n "
+                            + " 3 = Cedula del Cliente\n "
+                            + " 4 = Correo del Cliente\n "
+                            + " 5 = Numero del Cliente\n "));
+                } catch (HeadlessException | NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Ingreso opcion incorrecta");
+                }
+                switch (opcion) {
+                    case 1 -> {
+                        String nombre = JOptionPane.showInputDialog("Ingrese nuevo nombre");
+                        if (nombre.isEmpty()) {
+                            JOptionPane.showMessageDialog(null, "Texto vacio");
+                        } else {
+                            aux.getDato().setNombreCliente(nombre);
+                            JOptionPane.showMessageDialog(null, "Cambiado con exito");
+                        }
+                    }
+                    case 2 -> {
+                        String apellidos = JOptionPane.showInputDialog("Ingrese nuevos apellidos");
+                        if (apellidos.isEmpty()) {
+                            JOptionPane.showMessageDialog(null, "Texto vacio");
+                        } else {
+                            aux.getDato().setApellidosCliente(apellidos);
+                            JOptionPane.showMessageDialog(null, "Cambiado con exito");
+                        }
+                    }
+                    case 3 -> {
+                        int cedula = Integer.parseInt(JOptionPane.showInputDialog("Ingrese nueva cedula"));
+                        aux.getDato().setCedulaCliente(cedula);
+                        JOptionPane.showMessageDialog(null, "Cambiado con exito");
+                        JOptionPane.showMessageDialog(null, "Texto vacio");
+                    }
+                    case 4 -> {
+                        String correo = JOptionPane.showInputDialog("Ingrese nuevo correo");
+                        if (correo.isEmpty()) {
+                            JOptionPane.showMessageDialog(null, "Texto vacio");
+                        } else {
+                            aux.getDato().setCorreoCliente(correo);
+                            JOptionPane.showMessageDialog(null, "Cambiado con exito");
+                        }
+                    }
+                    case 5 -> {
+                        int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese nuevo numero"));
+                        aux.getDato().setNumCliente(numero);
+                        JOptionPane.showMessageDialog(null, "Texto vacio");
+                    }
+                }
             } else {
                 System.out.println("No existe");
             }
--- ProyectoFinalEDatosGrupo3/src/proyectofinaledatosgrupo3/ProyectoFinalEDatosGrupo3.java
+++ ProyectoFinalEDatosGrupo3/src/proyectofinaledatosgrupo3/ProyectoFinalEDatosGrupo3.java
@@ -8,13 +8,17 @@ public class ProyectoFinalEDatosGrupo3 {
 
     public static void main(String[] args) {
         ListaClientes l = new ListaClientes();
-        Cliente cliente = new Cliente();
         l.inserta(new Cliente("Esteban", "Sanarrucia Molina", 117410062, "esteban9926@hotmail.com", 60319926));
         l.inserta(new Cliente("Jose", "Perez Ortega", 117410892, "jose@hotmail.com", 60948504));
         System.out.println(l.toString());
         System.out.println(l.existe(3));
         l.elimina(1);
         l.inserta(new Cliente("Test", "Test test", 000000000000, "test@hotmail.com", 000000000));
+        System.out.println(l.toString());
+        System.out.println(l.extrae(3));
+        System.out.println("***************");
+        System.out.println(l.toString());
+        l.modificar(3);
         System.out.println(l.toString());
     }
 
